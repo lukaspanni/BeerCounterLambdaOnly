00c3317fc6860a4339e25920809754d693eab393
Lukas Panni <lukas-panni@web.de>
2019-10-23 07:05:41 +0200

    Code BeerCounterJava

---
+++ BeerCounterJava/src/BeerCounterJava/Counter.java
@@ -0,0 +1,81 @@
+package BeerCounterJava;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Counter {
+
+	private int counter = 0;
+	private long firstBeerSaved = -1;
+
+	public static void main(String[] args) {
+		Counter c = new Counter();
+		c.addBeers(15);
+		c.exit();
+	}
+
+	public Counter() {
+		readPresistentData();
+		checkTimeout();
+	}
+
+	public void exit() {
+		writePersistentData();
+	}
+
+	public void addBeers(int amnt) {
+		checkTimeout();
+		counter += amnt;
+		if (firstBeerSaved == -1) {
+			firstBeerSaved = System.currentTimeMillis();
+		}
+	}
+
+	public int getBeers() {
+		checkTimeout();
+		return counter;
+	}
+
+	private void checkTimeout() {
+		if (firstBeerSaved == -1) {
+			return;
+		}
+		long now = System.currentTimeMillis();
+		if (firstBeerSaved + (24 * 60 * 60 * 1000) < now) {
+			firstBeerSaved = -1;
+			counter = 0;
+		}
+	}
+
+	private void writePersistentData() {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("beers.txt", true))) {
+			String str = String.valueOf(firstBeerSaved) + "," + String.valueOf(counter);
+			bw.write(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readPresistentData() {
+		try (BufferedReader br = new BufferedReader(new FileReader("beers.txt"))) {
+			String line = br.readLine();
+			while (line != null) {
+				line = br.readLine();
+				parseLine(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void parseLine(String line) {
+		String[] split = line.split(",");
+		if (split.length == 2) {
+			firstBeerSaved = Long.parseLong(split[0]);
+			counter = Integer.parseInt(split[1]);
+		}
+	}
+
+}
